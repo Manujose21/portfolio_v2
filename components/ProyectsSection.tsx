6ba2113dd011a6e4189ab26b6e3346ca24c59aed
manujose21 <m4nufra@gmail.com>
2024-10-05 20:00:02 -0400

    change title

--- components/ProyectsSection.tsx
+++ components/ProyectsSection.tsx
@@ -6,7 +6,7 @@ export const ProyectsSection = () => {
         <section className="flex flex-col gap-4 max-w-xl mt-8 md:mt-32">
             <div className=' flex gap-4'>
                 <IoCodeSlash size={35}/>
-                <h1 className="text-2xl font-semibold">Proyectos</h1>
+                <h1 className="text-2xl font-semibold">Mis Proyectos</h1>
             </div>
         </section>
     )
