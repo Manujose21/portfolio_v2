429c68a9c002e60fc8c7968b6b8ba5e02c5e0646
manujose21 <m4nufra@gmail.com>
2024-10-10 18:41:52 -0400

    ignore .env

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
     return (
         <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
             <div className="flex items-center justify-center">
-                <p>My portfolio © { new Date().getFullYear() } </p>
+                <p className="font-semibold">Manuel Urdaneta protafolio © { new Date().getFullYear() } </p>
             </div>
         </footer>
     );
--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -4,7 +4,7 @@ import styles from "./hero_section.module.css";
 export const HeroSection = ({className}: {className?: string}) => {
     return (
         <section className={`flex flex-col gap-4 mt-8 md:mt-32 relative ${className}`}>
-            <IoCodeSlash size={75} className={`absolute top-0 right-0 ${styles.animation_icon}`}/>
+            <IoCodeSlash size={100} className={`absolute top-0 right-0 ${styles.animation_icon}`}/>
             <div className="max-w-xl">
                 <h1 className="text-5xl font-bold">Hey, soy Manuel 👋</h1>
                 <p className="text-xl font-medium mt-4"> 
